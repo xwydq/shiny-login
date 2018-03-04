a2ebe02444b60ba303fe0946e09dc6b1a6f50b4f
xwydq <xwy314dq@gmail.com>
2018-03-04 13:24:47 +0800

    format code

--- single-page/server.R
+++ single-page/server.R
@@ -1,14 +1,6 @@
 
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(dplyr)
-library(lubridate)
 
 
 shinyServer(function(session, input, output) {
--- single-page/ui.R
+++ single-page/ui.R
@@ -1,12 +1,5 @@
 
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
 library(shiny)
-library(shinydashboard)
 
 shinyUI(fluidPage(tags$head(tags$style(
   HTML("
